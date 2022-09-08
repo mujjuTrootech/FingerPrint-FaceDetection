4998a6d2f73d5ea7b5cd232f20a14305ae65fb8e
Aayushi <muzammil.modan@trootech.com>
2022-09-08 16:41:28 +0530

    no message

--- lib/utils/custom_colors.dart
+++ lib/utils/custom_colors.dart
@@ -3,8 +3,8 @@ import 'dart:ui';
 
 class CustomColors {
   static final Color firebaseNavy = Color(0xFF2C384A);
-  static final Color firebaseOrange = Color(0xFFF57C00);
-  static final Color firebaseAmber = Color(0xFFFFA000);
+  static final Color firebaseOrange = Color(0xFF2F7C00);
+  static final Color firebaseAmber = Color(0xFF1AA845);
   static final Color firebaseYellow = Color(0xFFFFCA28);
   static final Color firebaseGrey = Color(0xFFECEFF1);
   static final Color googleBackground = Color(0xFF4285F4);
